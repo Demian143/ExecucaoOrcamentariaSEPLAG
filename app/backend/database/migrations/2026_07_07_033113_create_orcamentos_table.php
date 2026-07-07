05f928741a2665b4604619137a521a169fe7a701
Demian143 <patrickxavier140@gmail.com>
2026-07-07 00:36:33 -0300

    Feat: Tabela de orçamentos

---
+++ app/backend/database/migrations/2026_07_07_033113_create_orcamentos_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orcamentos', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedSmallInteger('ano');
+            // Quem gasta
+            $table->foreignId('unidade_gestora_id')->constrained('unidades_gestoras');
+            // Para que
+            $table->foreignId('programa_id')->constrained('programas');
+            $table->foreignId('acao_id')->constrained('acoes');
+            $table->foreignId('subfuncao_id')->constrained('subfuncoes');
+            // Com o quê
+            $table->foreignId('natureza_despesa_id')->constrained('naturezas_despesa');
+            // De onde
+            $table->foreignId('fonte_recurso_id')->constrained('fontes_recurso');
+            // Valores
+            $table->decimal('dotacao_inicial', 15, 2)->default(0);
+            $table->decimal('dotacao_atualizada', 15, 2)->default(0);
+            $table->decimal('valor_empenhado', 15, 2)->default(0);
+            $table->decimal('valor_liquidado', 15, 2)->default(0);
+            $table->decimal('valor_pago', 15, 2)->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orcamentos');
+    }
+};
