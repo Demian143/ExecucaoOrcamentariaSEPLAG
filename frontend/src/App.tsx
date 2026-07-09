e0350dfbf2370b0bc1cbe56065ba7f0898a06d14
Demian143 <patrickxavier140@gmail.com>
2026-07-08 23:31:53 -0300

    Feat: Estrutura basica de roteamento para teste

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,18 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ProtectedRoute } from './components/ProtectedRoute';
+import Home from './pages/Home';
+import Login from './pages/Login';
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/login'/>
-        <Route element={<ProtectedRoute />}> </Route>
+        <Route path='/login' element={<Login/>} />
+        
+        <Route element={<ProtectedRoute />}> 
+          <Route path='/home' element={<Home />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,13 @@
+
+function Login() {
+    return (
+        <form>
+            <h1>Login</h1><br/>
+            <input type="text" name="login"/><br/>
+            <h1>Senha</h1><br/>
+            <input type="password" name="password" />
+        </form>
+    );
+}
+
+export default Login;
