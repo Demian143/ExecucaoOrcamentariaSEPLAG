53bec7fe90c8830355ebb0bb3f3819a2de0da136
Demian143 <patrickxavier140@gmail.com>
2026-07-08 21:28:39 -0300

    Feat: Adição de estrutura base do app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ProtectedRoute } from './components/ProtectedRoute';
+
 function App() {
 
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/login'/>
+        <Route element={<ProtectedRoute />}> </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
